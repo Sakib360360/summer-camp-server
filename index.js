a031e7304c58df6c9c0b400039cd01b20c13e3a2
Sakib360360 <saakibabrar@gmail.com>
2023-06-07 16:57:59 +0600

    classes and instructors routed defined

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ async function run() {
         const result = await classCollection.find().toArray()
         res.send(result)
     })
+    // get all instructors
+    app.get('/instructors',  async (req, res) => {
+        const result = await instructorCollection.find().toArray()
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
