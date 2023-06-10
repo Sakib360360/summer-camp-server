3fb4b5d01567fac120e2dda49c333f96c13c219a
Sakib360360 <saakibabrar@gmail.com>
2023-06-10 22:17:34 +0600

    vercel

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const classCollection = client.db('language-camp').collection('classes')
         const instructorCollection = client.db('language-camp').collection('instructors')
         const selectedClasses = client.db('language-camp').collection('selected-classes')
